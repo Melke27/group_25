6167f36e46ca818a724c6e03e5127d96a0ddd0b0
abdurazakm <abdulkem5472gmail.com>
2025-10-07 11:57:54 +0300

    working model is added

---
+++ backend/models/message.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  customerName: { type: String, required: true },
+  customerEmail: { type: String, required: true },
+  subject: { type: String, required: true },
+  message: { type: String, required: true },
+  status: { type: String, enum: ['unread', 'read'], default: 'unread' },
+  createdAt: { type: Date, default: Date.now },
+  relatedBookingId: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking' }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
